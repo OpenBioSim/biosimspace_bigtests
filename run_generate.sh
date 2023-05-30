7e4288cf7585c019c300b278d28244c03019227c
Matthew <matthew@openbiosim.org>
2023-05-30 16:43:57 +0100

    Initial commit of system generator. Requires openbabel in order to generate SMILES strings from ligand pdbs

---
+++ run_generate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -n 1
+    # Request 1 gpu per job
+#SBATCH --gres=gpu:1
+    # allocate 5 processors/CPUs per GPU
+#SBATCH --cpus-per-gpu 5
+    # allocate 5 GB of memory per job
+#SBATCH --mem 5000
+    # Set job name
+#SBATCH --job-name=genall
+#SBATCH --output=systemgenerate.o
+#SBATCH --error=systemgenerate.err
+
+source /home/matthew/mambaforge/bin/activate openbiosim
+python3 --version
+./convtoSMILES.sh
+python ~/bigtests_inprogresss/generate_systems.py 
+wait
+cd ..
+
